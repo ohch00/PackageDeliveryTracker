41a00e91965098191a647d4f58fdc370a22e9c3e
ohch00 <55716852+ohch00@users.noreply.github.com>
2021-09-02 16:17:30 -0700

    Added more formatting to main template; can now read txt file to get tracking numbers inputted by users.

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import os
 
 app = Flask(__name__)
 
+f = open('tracking_list.txt', 'r')
+content = f.read()
+
 
 @app.route('/')
 def root():
-    return None
+    return render_template("main.html", tracking_list=content)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
-    app.run(port=port)
+    app.run(port=port, debug=True)
