d63d795c2e75d80880515085d361fd8844124eb1
ohch00 <55716852+ohch00@users.noreply.github.com>
2021-09-12 20:55:40 -0700

    added update and delete buttons added form to add and filter tracking numbers

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from tracking_list import tracking_list
 import os
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def root():
     return render_template("main.html", tracking_list=tracking_list)
 
 
-@app.route('/add')
+# page to add new tracking numbers to sheet
+@app.route('/add', methods=['POST'])
 def add():
-    return 0
+    sender = request.form['sender'] or None
+    carrier = request.form['carrier'] or None
+    tracking_number = request.form['tracking_number'] or None
+    status = request.form['status'] or None
+
+    try:
+        return redirect('/')
+
+    except:
+        return "There was an issue adding the new tracking number. Please make sure the fields are not empty."
+
+# search/filter tracking numbers
+
+# delete tracking numbers
+
+# update tracking numbers
 
 
 if __name__ == "__main__":
