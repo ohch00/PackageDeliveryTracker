265d8ed35545765c9583003a2374da5272cf9291
ohch00 <55716852+ohch00@users.noreply.github.com>
2021-09-18 17:49:43 -0700

    changed tracking_list to txt file, open file as json

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
 from flask import Flask, request, render_template, redirect
-from tracking_list import tracking_list
 import os
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def root():
-    return render_template("main.html", tracking_list=tracking_list)
+    try:
+        with open("tracking_list.txt", 'r') as file:
+            data = file.read()
+            tracking_list = json.loads(data)
+
+        return render_template("main.html", tracking_list=tracking_list)
+
+    except:
+        with open("tracking_list.txt", 'a+') as file:
+            file.write('[]')
+            file.close()
 
 
 # page to add new tracking numbers to sheet
 @app.route('/add', methods=['POST'])
 def add():
+    sender = request.form['sender'] or None
+    carrier = request.form['carrier'] or None
+    tracking_number = request.form['tracking_number'] or None
+
+    try:
+        return redirect('/')
+
+    except:
+        return "There was an issue adding the new tracking number. Please make sure the fields are not empty."
+
+
+# search/filter tracking numbers
+def filter():
     sender = request.form['sender'] or None
     carrier = request.form['carrier'] or None
     tracking_number = request.form['tracking_number'] or None
@@ -24,11 +47,15 @@ def add():
     except:
         return "There was an issue adding the new tracking number. Please make sure the fields are not empty."
 
-# search/filter tracking numbers
 
 # delete tracking numbers
+def delete():
+    pass
+
 
 # update tracking numbers
+def update():
+    pass
 
 
 if __name__ == "__main__":
